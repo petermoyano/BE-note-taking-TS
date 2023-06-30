dc7fa9017568096ada20c5c9faa6e7bbe46e727f
Peter M <pmoyano@solvd.com>
2023-06-30 16:43:27 -0300

    add postgres database url

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool;
+const config = require("dotenv").config;
+config();
 
 let pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "notes",
-    password: "postgres",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+    // user: "postgres",
+    // host: "localhost",
+    // database: "notes",
+    // password: "postgres",
+    // port: 5432,
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const notesRoutes = require("./src/notes/routes");
+const pool = require("./db");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Home of BE");
+});
+
+app.get("/ping", async (req, res) => {
+    const result = await pool.query("SELECT NOW()");
+    return res.send(result.rows[0]);
 });
 
 app.use("/notes", notesRoutes);
